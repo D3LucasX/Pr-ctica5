0b58c141423e162635f1475c7765e9b91f99103e
D3LucasX <d3lucasx@gmail.com>
2024-11-08 22:07:59 +0100

    Ejercicio 1 retocado

--- Ejercicios/Ejercicio_1.c
+++ Ejercicios/Ejercicio_1.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-	
 
 
-	int main(){
+
+int main(){
 
 	int inventario[2][3][4][5][10]={0};
 
@@ -13,19 +13,19 @@
 
 	inventario[1][1][1][3][2] = 120;
 
-	inventario[0][2][4][4][3] = 300; 
+	inventario[0][2][3][4][3] = 300; 
 
-	inventario[1][0][3][5][4] = 100;
+	inventario[1][0][3][4][4] = 100;
 
-	inventario[0][1][0][6][5] = 250;
+	inventario[0][1][0][4][5] = 250;
 
-printf("Inventario en america,sede 1,categoria televisores,modelo 0,lote 0:%d unidades\n",inventario[0][0][0][0][0]);
-printf("Inventario en europa,sede 2,categaria telefonos,modelo 3,lote 2:%d unidades\n",inventario[1][1][1][3][2]);
-printf("Inventario en america ,sede 3,categoria ordenadores,modelo 4,lote 3:%d unidades\n",inventario[0][2][4][4][3]);
-printf("Inventario en europa ,sede 4,categoria electrodomesticos,modelo 5,lote 4:%d unidades\n",inventario[1][0][3][5][4]);
-printf("Inventario en america ,sede 5,categoria telefonos,modelo 6,lote 5: %d unidades\n",inventario[0][1][0][6][5]);
+	printf("Inventario en america, sede 0, categoria televisores, modelo 0, lote 0: %d unidades\n", inventario[0][0][0][0][0]);
+	printf("Inventario en europa, sede 1, categaria telefonos, modelo 3, lote 2: %d unidades\n", inventario[1][1][1][3][2]);
+	printf("Inventario en america, sede 2, categoria ordenadores, modelo 4, lote 3: %d unidades\n", inventario[0][2][3][4][3]);
+	printf("Inventario en europa, sede 0, categoria ordenadores, modelo 4, lote 4: %d unidades\n", inventario[1][0][3][4][4]);
+	printf("Inventario en america, sede 1, categoria televisores, modelo 4, lote 5: %d unidades\n", inventario[0][1][0][4][5]);
 
-return 0;
+	return 0;
 
 }
 

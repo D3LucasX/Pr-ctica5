67c97871e75a85dd346ec9c720efbd8d0dbf05b1
Jose Maria De Lucas Plata <d3lucasx@gmail.com>
2025-01-22 14:26:51 +0100

    Ejercicio 1 acabado

--- Práctica_7_JoseMariaDeLucasPlata/Práctica_7_JoseMariaDeLucasPlata.c
+++ Práctica_7_JoseMariaDeLucasPlata/Práctica_7_JoseMariaDeLucasPlata.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 /*
  *
  * 	Autor: Jose Maria De Lucas Plata.
  *
  */
+void CantidadNumeros(int* total_numeros);
+void PedirNumeros(int* numerosAordenar, int total_numeros);
+void OrdenarNumeros(int* numerosAordenar, int total_numeros);
+
 int main (){
 	char volverAcomparar [2];
 	int total_numeros;
@@ -22,18 +27,18 @@ int main (){
     		printf("ERROR, NO HAY MEMORIA\n");
     		return EXIT_FAILURE;
     		} //Si no hay espacio para hacer la reserva de la memória, entonces se cancelara.
-		}elseif (numerosAordenar != null){
-
+		}else if (numerosAordenar != NULL){
+			numerosAordenar = (int*) realloc(numerosAordenar,(total_numeros * sizeof(int)));
 		}
 		PedirNumeros(numerosAordenar, total_numeros);
+		OrdenarNumeros(numerosAordenar, total_numeros);
 		printf("¿Desea volver a comparar una serie de números?");
-		scanf(" %[^\n]" volverAcomparar);
+		scanf(" %[^\n]", volverAcomparar);
 	}
-	while (strcmp(volverAcomparar, "si") == 0){
-    }
+	while (strcmp(volverAcomparar, "si") == 0);
 
 
-
+	free (numerosAordenar);
 	return EXIT_SUCCESS;
 
 }
@@ -42,29 +47,30 @@ void CantidadNumeros(int* total_numeros){
 
 
 	printf("¿Cuantos números desea ordenar?.\n");
-	scanf("%d", total_numeros);
-	printf("%d", *total_numeros);
+	scanf(" %d", total_numeros);
+	printf("El total de números es %d\n", *total_numeros);
 }
-void PedirNumeros(int* numerosAordenar, int tamaño){
-	printf("Introduzca la serie de números\n.");
-	for (i = 0; i < total_numeros ; i++);
-		scanf("%d", numerosAordenar[i]);
+void PedirNumeros(int* numerosAordenar, int total_numeros){
+	int i = 0;
+	printf("Introduzca la serie de números de uno en uno.\n");
+	for (i = 0; i < total_numeros ; i++){
+		scanf("%d", &numerosAordenar[i]);
+	}
 }
 void OrdenarNumeros(int* numerosAordenar, int total_numeros){
-	int i,j, temporal;
+	int i,j, box;
 	for(i = 0; i < total_numeros ; i++){
-		temporal = numerosAordenar[i];
-			for(j = 1; j < total_numeros ; j++){
-				if (temporal > numerosAordenar[j]){
-					temporal = numerosAordenar[j];
-					printf("%d es menor que %d.\n", temporal, numerosAordenar[j]);
+		
+			for(j = i + 1; j < total_numeros ; j++){
+				if (numerosAordenar[i] < numerosAordenar[j]){
+					box = numerosAordenar[i];
+					numerosAordenar[i] = numerosAordenar[j];
+					numerosAordenar[j] = box;
 				}
 			}
-			printf("%dº - %d", i, temporal);
+			//printf("%dº - %d , ", i +1, numerosAordenar[i]);
 		}	
+		for (i = 0 ; i < total_numeros; i++){
+		printf(" %d\n",numerosAordenar[i]);
 	}
-
-	/* To do:
-		-Hacer el realloc.
-		-ordenar el dowhile
-		-Probar
+	}

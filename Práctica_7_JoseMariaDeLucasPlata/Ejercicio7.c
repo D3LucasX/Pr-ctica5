ad40e579aa6c996cb6b63fe2866848b6cd54fba1
Marcos1304E <marcosescamilla1304@gmail.com>
2025-02-06 21:23:20 +0100

    e7

--- Práctica_7_JoseMariaDeLucasPlata/Ejercicio7.c
+++ Práctica_7_JoseMariaDeLucasPlata/Ejercicio7.c
@@ -6,6 +6,8 @@
  * 	Autor: Jose Maria De Lucas Plata.
  *
  */
+
+
 void EsPrimo(int* array, int num){
 
 	for (int i = 0; i < num; i++){
@@ -22,22 +24,34 @@ void EsPrimo(int* array, int num){
 		}
 	}
 int main (){
+
 	int num = 0;
-	while (num < 10){
+	while (num <= 10){
 		printf("Dame un número mayor de 10\n", num);
 		scanf("%d", &num);
 	}
+
 	int* array = (int*) malloc (num * sizeof(int));
-	for(int i = 0; i < num; i++){
-		array[i] = i;
+
+	if (array == NULL)
+	{
+		printf("Error al asignar memoria.\n");
+		return 1;
 	}
-	EsPrimo(array, tamano);
-	for(int i = 0; i < ){
 
+	EsPrimo(array, num);
+	/*for(int i = 0; i < ){
+
+	}*/
+
+	for(int i = 0; i <= num; i++){
+		if (array[i] == 1)
+		{
+			printf("%d",i);
+		}
 	}
 
-
-
-	return EXIT_SUCCESS;
+free(array);
+	return 0;
 
 }
